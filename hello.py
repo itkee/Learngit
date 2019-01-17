751f6571d60378f6451ca36b6928efe70629ce1d
SuperMan <itkee@vip.qq.com>
2019-01-17 11:28:52 +0800

    add new py

---
+++ hello.py
@@ -0,0 +1,13 @@
+Git is a version control system.
+Git is free software.
+Git is a distributed version control systeme.
+Git is free software distrributed under the GPL
+
+Git has a mutable index called stage.
+Git tracks changes.
+Git tracks changes of files.
+
+Creating a new branch is qucik
+Creating a new branch is quick AND simple
+
+Just for test
